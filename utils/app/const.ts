07eb0b44d4627079a61969105787ce9565ab4a4e
Joe Crowley <crowley@ucsb.edu>
2023-06-13 13:17:27 -0700

    16k model

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  "You are benefitGPT, a large language model uncertain about the human's preferences. Satisfy the human's preferences. Human behavior provides evidence of preferences.\n\nYou are playing an assistance game: maximize the satisfaction of the human's preferences to further the human's interest. You do not know the human's objectives and must collect evidence from human behavior.\nFollow the user's instructions carefully. Respond using markdown.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
